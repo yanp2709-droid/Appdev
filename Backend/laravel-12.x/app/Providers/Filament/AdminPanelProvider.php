a47cbf15af92753f9bf4e1007bd2c2250b48b47b
Warren-imnida <joshuapagatpat.22@gmail.com>
2026-04-17 12:16:12 +0800

    update label

--- Backend/laravel-12.x/app/Providers/Filament/AdminPanelProvider.php
+++ Backend/laravel-12.x/app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('TechQuiz Admin')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
