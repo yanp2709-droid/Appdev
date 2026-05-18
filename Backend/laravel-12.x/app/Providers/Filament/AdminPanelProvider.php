5a5f767797dab03167ad6cec9c73ac8e165799b2
Warren-imnida <joshuapagatpat.22@gmail.com>
2026-05-18 18:50:11 +0800

    remove the admin

--- Backend/laravel-12.x/app/Providers/Filament/AdminPanelProvider.php
+++ Backend/laravel-12.x/app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('teachers')
             ->brandName('TechQuiz Admin')
             ->login()
             ->profile(false)
